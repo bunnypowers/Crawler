5a209a4a7370e8cec02725a565e376274dd31061
bunnypowers69 <bunnypowers4991@gmail.com>
2025-09-28 17:00:36 +0530

    implemented basic-crawler

--- src/main/java/org/example/Crawler.java
+++ src/main/java/org/example/Crawler.java
@@ -2,7 +2,10 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.HashSet;
 
 public class Crawler {
@@ -19,11 +22,21 @@ public class Crawler {
             return;
         }
         depth++;
-        Document document = Jsoup.connect(url).timeout(5000).get();
-        //Indexer
-        System.out.println(document.title());
+        try {
+            Document document = Jsoup.connect(url).timeout(5000).get();
+            //Indexer
+            System.out.println(document.title());
+            Elements availableLinksOnPage = document.select("a[href]");
+            for (Element currentLink : availableLinksOnPage) {
+                getPageLinks(currentLink.attr("abs:href"), depth);
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+
     }
     public static void main(String[] args) {
-
+        Crawler crawler = new Crawler();
+        crawler.getPageLinks("https://www.tpointtech.com/", 1);
     }
 }
